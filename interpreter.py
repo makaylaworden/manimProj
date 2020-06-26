4409dcb216157512d2c4b138be69285bc1b8141f
makaylaworden <46685092+makaylaworden@users.noreply.github.com>
2020-06-26 02:03:56 -0700

    More animations added

--- interpreter.py
+++ interpreter.py
@@ -10,6 +10,8 @@
 #   python manim.py interpreter.py interp
 
 from manimlib.imports import *
+from myVMobjects import Arrayo
+from myVMobjects import Stacko
 
 # This is the main function, it calls everything it needs
 # to in order to produce an animation similar to interpretor.mp4
@@ -18,6 +20,8 @@ class interp (Scene):
         introduction(self)
         instruct_box(self)
         make_stack(self)
+        code_box(self)
+        subtitle(self)
 
 
 # introduction displays the first 2 slides of
@@ -36,55 +40,99 @@ def introduction(self): # Called first
     self.wait()
     self.play(ShowCreation(defin), Transform (intro, defin))
     self.wait()
-    self.play(FadeOut(defin))
+    self.play(FadeOutAndShiftDown(defin))
 
 # This will show the purple box of instructions
 # in interpreter.mp4 in the lower left corner
 def instruct_box (self): # Called second
-    push20 = Rectangle()
-    push20.set_fill(PURPLE, opacity = 1)
-    push22 = Rectangle()
-    push22.set_fill(PURPLE, opacity = 1)
-    add = Rectangle()
-    add.set_fill(PURPLE, opacity = 1)
-    store = Rectangle()
-    store.set_fill(PURPLE, opacity = 1)
+    text = [TextMobject("VPC\npush 20", tex_to_color_map={"push 20": BLACK}),
+            TextMobject("push 22", tex_to_color_map={"push 22": BLACK}),
+            TextMobject("add", tex_to_color_map={"add": BLACK}),
+            TextMobject("store", tex_to_color_map={"store": BLACK})]
+    # This is what is "stored" in the Array
+    
+    meArr = Arrayo.Array().build(len(text), PURPLE, text)
 
-    group = VGroup(push20, push22, add, store)
-    group.arrange(RIGHT)
-    group.to_corner(DOWN + RIGHT)
-    group.set_width(FRAME_WIDTH / 2)
+    meArr.to_corner(DOWN + RIGHT)
+    meArr.set_width(FRAME_WIDTH / 2) # this is a rando number
 
-    self.add(group)
-    self.play(ShowCreation(group))
-    self.wait()
+    self.add(meArr)
+    self.play(ShowCreation(meArr))
 
 # This will show the orange stack on the bottom
 # right corner of interpreter.mp4.
 def make_stack(self): # Called third
-    title = TextMobject("Stack")
-    box1 = Rectangle()
-    box2 = Rectangle()
-    box3 = Rectangle()
-    box1.set_fill(ORANGE, opacity = 1)
-    box2.set_fill(ORANGE, opacity = 1)
-    box3.set_fill(ORANGE, opacity = 1)
+    empty_text = [TextMobject(""), TextMobject(""), TextMobject("SP", tex_to_color_map={"SP": BLACK})]
+    stacko = Stacko.Stack().build(len(empty_text), ORANGE, empty_text)
+    title = TextMobject("STACK") # label on top of the stack
 
-    group = VGroup(title, box1, box2, box3)
-    group.arrange(DOWN) # you may end up changing this to UP, not sure
+    group = VGroup(title, stacko)
+    group.arrange(DOWN)
     group.to_corner(TOP + RIGHT)
-    group.set_width(FRAME_WIDTH / 5)
+    group.set_width(FRAME_WIDTH / 8) # this is a rando number
 
     self.add(group)
     self.play(ShowCreation(group))
-    self.wait()
+
     
-# instructions is the lefthand side of interpreter.mp4
+# code_box is the lefthand side of interpreter.mp4
 # where there displas code for add, push, and store.
-#def instructions(self):
-    # TODO: ME!
-    # GET BOXES (MAYBE GRID?)
-    #BOZES IN HGROUP!!!!!!!!!!!!!!!
-    # GET COLORS
-    # GET TEXT ON BOXES
-    # GET ARROWs
+def code_box(self): # Called fourth
+    # Make that top yellow label box:
+    label_box = Rectangle(fill_color=YELLOW, fill_opacity=1)
+    label_txt = TextMobject("CODE[VPC]", tex_to_color_map={"CODE[VPC]": BLACK})
+    label_txt.bg = BackgroundRectangle(label_txt, fill_color=YELLOW, fill_opacity=1)
+    label_group = VGroup(label_box, label_txt.bg, label_txt)
+
+    # Make blue code stack
+    text1 = TextMobject("add:{\n\tpush(pop()+pop());\n\tVPC++;}",
+                        tex_to_color_map={"add:{\n\tpush(pop()+pop());\n\tVPC++;}": BLACK})
+    text2 = TextMobject("push:{\n\tpush(CODE[VPC+1]);\n\tVPC+=2}",
+                        tex_to_color_map={"push:{\n\tpush(CODE[VPC+1]);\n\tVPC+=2}": BLACK})
+    text3 = TextMobject("store:{\n\tMem[CODE[VPC+1]]=pop();\n\tVPC+=2}",
+                        tex_to_color_map={"store:{\n\tMem[CODE[VPC+1]]=pop();\n\tVPC+=2}": BLACK})
+    text = [text1, text2, text3] # This is what will be displayed on the boxes
+    code_stack = Stacko.Stack().build(len(text), BLUE, text)
+
+    group = VGroup(label_group, code_stack)
+    group.arrange(DOWN)
+    group.set_width(FRAME_WIDTH/4) # this is a rando number
+    group.to_corner(UP + LEFT)
+
+    self.add(group)
+    self.play(ShowCreation(group))
+
+# subtitle is maintaining different comments on what
+# is happening on the screen
+def subtitle (self): # Called fifth
+    say = TextMobject( "Here's an interpreter for the program " +
+                       "x = 20 + 22. It only supports 3 " +
+                       "instructions: push, add, and store" )
+    say2 = TextMobject( "Let's execute the program! The first instruction " +
+                        "is 'push 20'. We jump to the instruction handler " +
+                        "for 'push', push 20 on the stack, and advance the " +
+                        "virtual program counter (VPC)")
+
+    main_box = Square(side_length=2)
+    label_txt1 = TextMobject("main()")
+    label_txt1.scale(.5)
+    label_txt2 = TextMobject("     int x=99;")
+    label_txt2.scale(.5)
+    label_txt3 = TextMobject ("     x=20+22; }")
+    label_txt3.scale(.5)
+    # TODO learn Latex lol
+
+    label_txt = VGroup(label_txt1, label_txt2, label_txt3)
+    label_txt.arrange(DOWN)
+    
+    label_txt.bg = BackgroundRectangle(label_txt)
+    main_group = VGroup(main_box, label_txt.bg, label_txt)
+    main_group.to_corner(UP + RIGHT)
+    
+    say.scale(.5)
+    say2.scale(.5)
+    say.to_corner(TOP + RIGHT)
+    self.play(Write(say))
+    self.wait()
+    self.play(FadeOut(say), FadeIn(say2), ShowCreation(main_group))
+    
